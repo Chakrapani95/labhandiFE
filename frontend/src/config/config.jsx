e995eaf771f2b7998b6f9eddb7c7a57e462c5bf3
Chakrapani95 <99731013+Chakrapani95@users.noreply.github.com>
2024-05-29 12:55:31 +0530

    Update config.jsx
    localhost

--- frontend/src/config/config.jsx
+++ frontend/src/config/config.jsx
@@ -1,8 +1,8 @@
-// export const backendUrl = 'http://localhost:4543/';
-// export const backendApiUrl = 'http://localhost:4543/api/';
+ export const backendUrl = 'http://localhost:4543/';
+ export const backendApiUrl = 'http://localhost:4543/api/';
 
-export const backendUrl = 'https://labhandibe.techjainsupport.co.in/';
-export const backendApiUrl = 'https://labhandibe.techjainsupport.co.in/api/';
+// export const backendUrl = 'https://labhandibe.techjainsupport.co.in/';
+// export const backendApiUrl = 'https://labhandibe.techjainsupport.co.in/api/';
 
 
 // export const backendUrl = 'http://appbe.techjainsupport.co.in/';
